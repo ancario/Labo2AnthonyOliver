eb3579364af6c21b634421a2646397317185175c
ancario <anthonylabbe68@gmail.com>
2024-10-01 14:55:48 -0400

    c
    c

--- MathUtilities.js
+++ MathUtilities.js
@@ -1,18 +1,54 @@
-export function Addition(valeur1, valeur2) {
-    return valeur1 + valeur2;
+export function Addition(x, y) {
+    return x + y;
 }
 
-export function Soustraction(valeur1, valeur2) {
-    return valeur1 - valeur2;
+export function Soustraction(x, y) {
+    return x - y;
 }
 
-export function Division(valeur1, valeur2) {
-    if (valeur2 === 0) {
+export function Multiplication(x, y) {
+    return x * y;
+}
+
+export function Division(x, y) {
+    if (y === 0) {
         throw new Error("Division par zéro n'est pas permise");
     }
-    return valeur1 / valeur2;
+    return x / y;
 }
 
-export function Multiplication(valeur1, valeur2) {
-    return valeur1 * valeur2;
+export function Modulo(x, y) {
+    return x % y;
 }
+
+export function Factorielle(n) {
+    if (n < 0) {
+        throw new Error("La factorielle n'est pas définie pour les nombres négatifs");
+    }
+    let resultat = 1;
+    for (let i = 1; i <= n; i++) {
+        resultat *= i;
+    }
+    return resultat;
+}
+
+export function EstPremier(n) {
+    if (n <= 1) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+export function NPremier(n) {
+    let count = 0;
+    let num = 2;
+    while (count < n) {
+        if (EstPremier(num)) {
+            count++;
+        }
+        num++;
+    }
+    return num - 1;
+}
+``
