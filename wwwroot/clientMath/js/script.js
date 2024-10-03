f505cc5e5963362e2c06579e6e56aec017a46d5e
chocobeaver <106833828+chocobeaver@users.noreply.github.com>
2024-10-03 10:52:08 -0400

    a

--- wwwroot/clientMath/js/script.js
+++ wwwroot/clientMath/js/script.js
@@ -1,5 +1,27 @@
 
 
-array.forEach(element => {
+async function TestMaths(url,param,showresult){
+    val=showresult(url+param)
+    fetch(url+param)
+        .then((response)=>{
+            check=""
+            if(val===null){
+                if(response.error!=null){
+                    check="OK"
+                }
+                else{
+                    check="ERROR"
+                }
+            }
+            else{
+                if(val===response.value){
+                    check="OK"
+                }
+                else{
+                    check="ERROR"
+                }
+            }
+            return 
+        })
     
-});
+}
